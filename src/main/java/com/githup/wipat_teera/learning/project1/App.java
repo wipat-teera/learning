8e2fd384d3ac9da9555fabd4d64a89ce4a8355f9
wipat teeraumnuaykul <wipat.teera@gmail.com>
2017-03-06 22:30:18 +0700

    First Commit

---
+++ src/main/java/com/githup/wipat_teera/learning/project1/App.java
@@ -0,0 +1,13 @@
+package com.githup.wipat_teera.learning.project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
